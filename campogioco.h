3956d448adff7539b0ea559e52494fd39a8f4d91
nowhereb0y <36408641+nowhereb0y@users.noreply.github.com>
2018-02-18 18:42:02 +0100

    Create campogioco.h

---
+++ campogioco.h
@@ -0,0 +1,8 @@
+#ifndef _campogioco_h
+#define _campogioco_h
+#define COLONNE 7
+#define RIGHE 6
+
+void riempi_campo(char campoGioco[][COLONNE]); //funzione per inizializzare la matrice
+void stampa_campo(char campoGioco[][COLONNE]); //funzione per stampare la matrice
+#endif

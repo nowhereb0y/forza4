5a02f5a2ebd753a9d2e42cffb88ea955c3d8bfa6
nowhereb0y <36408641+nowhereb0y@users.noreply.github.com>
2018-03-13 22:33:26 +0100

    Update stampe.c

--- src/stampe.c
+++ src/stampe.c
@@ -1,27 +1,32 @@
 /*
  * stampe.c
  *
- *  Created on: 18 feb 2018
- *      Author: nowhereboy
+ *
+ *      Autore: Salvatore Albino
  */
 
+#include <stdio.h>
 
->>>>>>> branch 'master' of https://github.com/nowhereb0y/forza4
-void banneriniziale()
+
+void banneriniziale() //procedura che stampa il banner di benvenuto
 
 	{
-	printf("███████╗ ██████╗ ██████╗ ███████╗ █████╗     ██╗  ██╗\n");
-	printf("██╔════╝██╔═══██╗██╔══██╗╚══███╔╝██╔══██╗    ██║  ██║\n");
-	printf("█████╗  ██║   ██║██████╔╝  ███╔╝ ███████║    ███████║\n");
-	printf("██╔══╝  ██║   ██║██╔══██╗ ███╔╝  ██╔══██║    ╚════██║\n");
-	printf("██║     ╚██████╔╝██║  ██║███████╗██║  ██║         ██║\n");
-	printf("╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝         ╚═╝\n");
-	printf("\n\n\n\n");
+	printf("\t\t _______ _______ _______ _______ _______     _______     _______ _______ \n");
+	printf("\t\t|\\     /|\\     /|\\     /|\\     /|\\     /|   |\\     /|   |\\     /|\\     /|\n");
+	printf("\t\t| +---+ | +---+ | +---+ | +---+ | +---+ |   | +---+ |   | +---+ | +---+ |\n");
+	printf("\t\t| |   | | |   | | |   | | |   | | |   | |   | |   | |   | |   | | |   | |\n");
+	printf("\t\t| |F  | | |O  | | |R  | | |Z  | | |A  | |   | |4  | |   | |!  | | |!  | |\n");
+	printf("\t\t| +---+ | +---+ | +---+ | +---+ | +---+ |   | +---+ |   | +---+ | +---+ |\n");
+	printf("\t\t|/_____\\|/_____\\|/_____\\|/_____\\|/_____\\|   |/_____\\|   |/_____\\|/_____\\|\n");
 
+	printf("\n\n\n\n");
+	printf("\t\tBenvenuti nel gioco Forza 4!\n");
+	printf("\t\tLo scopo del gioco è quello di allineare 4 pedine in orizzontale, verticale o in una delle due diagonali\n");
+	printf("\t\tBuon divertimento!\n\n");
 	}
 
 
-void bannervittoria()
+void bannervittoria() //procedura che stampa un banner in caso di vittoria
 
 	{
 	printf(" _______ _______ _______     _______ _______ _______ _______ _______     _______ \n");

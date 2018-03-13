1d41d95acdb155c3a51b410120916bc047a1f08d
nowhereb0y <36408641+nowhereb0y@users.noreply.github.com>
2018-03-13 22:33:51 +0100

    Update forza4.c

--- src/forza4.c
+++ src/forza4.c
@@ -36,10 +36,11 @@ int main()
 
 {
 	int mossaplayer1, mossaplayer2; //variabile per memorizzare la mossa del giocatore
-	char symbol;
+	char symbol;	//variabile utilizzata per passare il simbolo
 	int count = 0; //variabile utilizzata nel ciclo while per tenere conto dei tentativi errati ed uscire nel caso in cui la scelta sia sbagliata per 5 volte
-	int pareggi = 0;
-	int partite = 0;
+	int pareggi = 0; //variabile intera utilizzata come contatore delle partite terminate senza vincitore
+	int partite = 0; //variabile intera utilizzata come contatore per permettere ai giocatori di iniziare la partita una volta ciascuno
+
 	banneriniziale(); //stampa il banner di benvenuto
 
 	pl player1; //definisco il giocatore 1 come variabile  "pl" definito nella parte dichiarativa del programma
@@ -71,28 +72,29 @@ int main()
 	{
 		if (mosse==COLONNE*RIGHE||mosse==(1+(2*COLONNE*RIGHE))) //verifico che le mosse non siano superiori agli spazi contenuti nella matrice, nel caso lo siano termino la partita con un pareggio.
 				{
+					stampa_campo(campoGioco);
 					printf("\n\n\t\tPartita terminata, non ci sono vincitori!\n\n");
-					partite++;
-					pareggi++;
-					fineturno(p1, p2, pareggi);
-					vittoria=-3;
+					partite++; //aumento il contatore delle partite
+					pareggi++; //aumento il contatore dei pareggi
+					fineturno(p1, p2, pareggi); //richiamo
+					vittoria=-3; //imposto il valore di vittoria al valore -3 (è un valore scelto arbitrariamente, mi basta che sia diverso da -1 per far scattare l'ultima if all'interno di questo ciclo while)
 
 				}
 
 		else if (mosse % 2 == 0) //turno del giocatore 1
 				{
 					mossaplayer1=chiediposizione(campoGioco, p1); //chiamo la funzione chiediposizione che chiederà all'utente di scegliere la colonna dove posizionare la pedina, informazione che sarà restituita dalla funzione e memorizzata nella variabile "mossaplayer1"
-					symbol=p1->symbol;
+					symbol=p1->symbol; //valorizzo la variabile symbol con il simbolo assegnato al giocatore 1. Utilizzerò la variabile appena inizializzata per passarla come valore alla procedura mettipedina
 					mettipedina(mossaplayer1, symbol, campoGioco); //chiamo la procedura mettipedina che inserirà il simbolo dell'utente nella posizione scelta
 					mosse++; //incremento il contatore delle mosse
 					vittoria=checkwin(campoGioco, p1); //chiamo la funzione checkwin per verificare se ci sono 4 pedine allineate
-					if (vittoria != -1)
+					if (vittoria != -1)//nel caso in cui riscontro la vittoria da parte del giocatore eseguo le seguenti azioni
 							{
-								p1->vittorie++;
-								partite++;
-								stampa_campo(campoGioco);
-								bannervittoria();
-								fineturno(p1, p2, pareggi);
+								p1->vittorie++; //incremento il contatore delle vittorie sulla variabile strutturata del giocatore
+								partite++; //incremento il contatore delle partite
+								stampa_campo(campoGioco); //stampo la matrice per far controllare ai giocatori il campo di gioco
+								bannervittoria(); //stampo il messaggio di vittoria
+								fineturno(p1, p2, pareggi); //chiamo la procedura fineturno che stampa il numero di vittorie per ciascun giocatore ed il numero di pareggi
 
 							}
 
@@ -117,10 +119,10 @@ int main()
 
 					}
 
-		if (vittoria != -1)
+		if (vittoria != -1) //se c'è una vittoria o un pareggio
 				{
 				printf("\n\n\nVuoi giocare ancora????");
-				vittoria=rivincita(&count, campoGioco, partite, &mosse);
+				vittoria=rivincita(&count, campoGioco, partite, &mosse); //chiamo la funzione per chiedere all'utente se vuole giocare ancora o se vuole uscire
 				}
 			}
 
@@ -130,11 +132,11 @@ int main()
 
 	}
 
-void datigiocatori(pl * p1) //procedura richiamata per far inserire il nome all'utente nella variabile di tupo strutturato pl
+void datigiocatori(pl * p1) //procedura richiamata per far inserire il nome all'utente nella variabile di tipo strutturato pl
 
 	{
-		printf ("  Inserisci il tuo nome (max 25 caratteri):");
-		scanf("%s", p1->nome);
+		printf ("  Inserisci il tuo nome (max 25 caratteri): ");
+		scanf("%s", p1->nome); //memorizzo il nome inserito dal giocatore nella variabile strutturata contenente i dati del giocatore
 
 	}
 
@@ -173,8 +175,7 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 	{
 	int i,j;
 	int ritorno= -1;
-	//char symbol;
-	//symbol=p1->symbol;
+
 
 
 	//Check orizzontale
@@ -182,7 +183,7 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 		{
 		for (j=0; j<COLONNE;j++)
 			{
-				if ( j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[i][j+1]==p1->symbol && campoGioco[i][j+2]==p1->symbol && campoGioco[i][j+3]==p1->symbol)
+				if ( j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[i][j+1]==p1->symbol && campoGioco[i][j+2]==p1->symbol && campoGioco[i][j+3]==p1->symbol) //oltre a verificare che ci siano le 4 pedine allineate orizzontalmente verifico che l'incremento della colonna non sia eccessivo
 					{
 						printf("\n\nVITTORIA ORIZZONTALE per %s!!!! \n", p1->nome );
 						ritorno=1;
@@ -195,7 +196,7 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 		{
 		for (j=0; j<COLONNE;j++)
 			{
-				if (i+3 <= RIGHE-1 &&  campoGioco[i][j]==p1->symbol && campoGioco[i+1][j]==p1->symbol && campoGioco[i+2][j]==p1->symbol && campoGioco[i+3][j]==p1->symbol)
+				if (i+3 <= RIGHE-1 &&  campoGioco[i][j]==p1->symbol && campoGioco[i+1][j]==p1->symbol && campoGioco[i+2][j]==p1->symbol && campoGioco[i+3][j]==p1->symbol) //oltre a verificare che ci siano le 4 pedine allineate verticalmente verifico che l'incremento della riga non sia eccessivo
 					{
 						printf("\n\nVITTORIA VERTICALE per %s!!!! \n", p1->nome );
 						ritorno=2;
@@ -207,7 +208,8 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 		{
 		for (j=0; j<COLONNE;j++)
 			{
-				if (i+3 >= RIGHE-1 && j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[i+1][j+1]==p1->symbol && campoGioco[i+2][j+2]==p1->symbol && campoGioco[i+3][j+3]==p1->symbol)
+				if (i+3 >= RIGHE-1 && j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[i+1][j+1]==p1->symbol && campoGioco[i+2][j+2]==p1->symbol && campoGioco[i+3][j+3]==p1->symbol) //oltre a verificare che ci siano le 4 pedine allineate diagonalmente verifico che l'incremento  di riga e colonna non sia eccessivo
+
 					{
 						printf("\n\nVITTORIA DIAGONALE DECRESCENTE per %s!!!! \n", p1->nome);
 						ritorno=3;
@@ -223,7 +225,7 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 			{
 
 
-				 if (i-3 >= 0 && j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[(i-1)][j+1]==p1->symbol && campoGioco[i-2][j+2]==p1->symbol && campoGioco[i-3][j+3]==p1->symbol)
+				 if (i-3 >= 0 && j+3 <= COLONNE-1 && campoGioco[i][j]==p1->symbol && campoGioco[(i-1)][j+1]==p1->symbol && campoGioco[i-2][j+2]==p1->symbol && campoGioco[i-3][j+3]==p1->symbol) //oltre a verificare che ci siano le 4 pedine allineate diagonalmente verifico che l'incremento o la sottrazione  di riga e colonna non sia eccessivo
 					{
 
 						printf("\n\nVITTORIA DIAGONALE CRESCENTE per %s!!!!\n", p1->nome);
@@ -236,24 +238,24 @@ int checkwin(char campoGioco[][COLONNE], pl * p1)
 
 
 
-void fineturno(pl * p1, pl * p2, int pareggi)
+void fineturno(pl * p1, pl * p2, int pareggi) //procedura che stampa il numero di vittorie ottenute dai due giocatori oltre al numero di pareggi
+
 	{
 		printf("\t\t %s , hai vinto %d partite!\n", p1->nome, p1->vittorie);
 		printf("\t\t %s , hai vinto %d partite!\n", p2->nome, p2->vittorie);
-		printf("\t\t Pareggi: %d", pareggi);
+		printf("\t\t Pareggi: %d\n\n", pareggi);
 	}
 
-int rivincita ( int * count,  char campoGioco[][COLONNE], int partite, int * mosse )
+int rivincita ( int * count,  char campoGioco[][COLONNE], int partite, int * mosse ) //funzione che chiede all'utente se desidera giocare nuovamente o se vuole uscire dal programma
 
 	{
 
 
-		int rivincita;
+		int rivincita; //variabile intera che utilizzo per memorizzare il valore inserito dall'utente
 		printf ("  Inserire 1 per effettuare una nuova partita oppure 2 per uscire\n\n");
-		printf ("  make your choice :)\n\n");
 		scanf("%d", &rivincita);
 
-			 if (rivincita != 1 && rivincita != 2 && (* count)>=4)
+			 if (rivincita != 1 && rivincita != 2 && (* count)>=4) //se la variabile rivincita è diversa da 1 e 2 avviso l'utente della scelta errata e incremento il contatore count passato tramite puntatore alla funzione. Raggiunti i 5 tentativi esco dal programma
 					{
 
 				printf ("  Hai effettuato una scelta errata per 5 volte, esco dal programma.\n");
@@ -271,32 +273,31 @@ int rivincita ( int * count,  char campoGioco[][COLONNE], int partite, int * mos
 					}
 
 
-			else if (rivincita==1)
+			else if (rivincita==1) // l'utente sceglie di rigiocare
 				{
-					riempi_campo(campoGioco);
-					if (partite % 2 == 0)
+					riempi_campo(campoGioco); //reinizializzo la matrice
+					if (partite % 2 == 0)	//se il numero di partite giocate è pari faccio iniziare il giocatore 1 inizializzando a 0 il contatore mosse (la differenza sta nel valorizzare mosse con un numero pari piuttosto che dispari, vedi i due else if del ciclo while principale)
 					{
 						* mosse=0;
 					}
 
 					else if (partite % 2 == 1)
 					{
-						* mosse=COLONNE*RIGHE+1;
+						* mosse=(COLONNE*RIGHE)+1; //se il numero di partite giocate è dispari faccio iniziare il giocatore 2
 					}
-				rivincita=-1;
+				rivincita=-1; //il valore che sarà tornato dalla funzione e memorizzato nel prigramma principale nella variabile guardia vittoria: in questo modo non esco dal ciclo principale while.
 
 
 				printf ("Si rigioca! \n");
-				printf ("RIVINCITA: %d\n", rivincita);
 					}
 
-			else if (rivincita==2)
+			else if (rivincita==2) //non si rigioca, esco
 					{
 
 				printf ("\n\n\t\t Grazie per aver giocato a Forza 4!");
 				exit(0);
 
 					}
-			return rivincita;
+			return rivincita; //ritorno il valore di rivincita
 
 	}
